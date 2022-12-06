4cfcb1215c635062b9c2aa22d2076ddf98b26aae
christine-jl <76869239+christine-jl@users.noreply.github.com>
2022-12-06 09:24:25 +0100

    Create Tile.java
    Creation de la méthode pour la gestion des tuiles

---
+++ Splendor_Game/src/Tile.java
@@ -0,0 +1,48 @@
+import java.util.Map;
+import java.util.Objects;
+
+public class Tile{
+
+    private final int prestiges;
+    private final String name; 
+    private final Map<Token, Integer> ressources;
+
+    public Tile(String name, int prestiges, Map<Token, Integer> ressources) {
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(ressources);
+        this.prestiges = prestiges;
+        this.name = name;
+        this.ressources = ressources;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        Objects.requireNonNull(other);
+        
+        return other instanceof Tile c
+        && this.prestiges == c.prestiges
+        && this.ressources.equals(c.ressources);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.name, this.prestiges, this.ressources);
+    }
+    
+
+    @Override
+    public String toString() {
+        return "\nName : " + this.name + "\n" +
+        "Prestiges : " + this.prestiges + "\n" +
+        "Ressources : " + Shell.toStringTokenSmallFormat(this.ressources, "  ") + "\n----------";
+    }
+
+    /**
+     * Getter method for an Object of type Tile.
+     * @return Number of prestiges of the tile.
+     */
+    public int getPrestiges() {
+        return this.prestiges;
+    }
+    
+}
