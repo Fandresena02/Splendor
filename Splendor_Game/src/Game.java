80727f794eae30034ed8dd449102033f7c187952
christine-jl <76869239+christine-jl@users.noreply.github.com>
2022-12-17 15:18:50 +0100

    Update Game.java
    Add some more modification of methods 
    Add comments on each methods

--- Splendor_Game/src/Game.java
+++ Splendor_Game/src/Game.java
@@ -11,6 +11,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+/**
+ * Class Game, manage game cases.
+ *
+ * @author Christine LI - Clement LUC - Fandresena RAKOTOMAHEFA
+ * @date 18/12/2022
+ * @file Game.java
+ */
 public class Game {
 
     private final static int MAXPLAYERS = 4;
@@ -23,7 +30,7 @@ public class Game {
     private final static String ACTIONBUYCARD = "buycard";
     private final static String ACTIONRESERVECARD = "reservecard";
 
-    private final Rules rules;
+     private final Rules rules;
     private final int nbPlayers;
     private final ArrayList<Player> players;
     private final LinkedHashMap<Token, Integer> countersStack;
@@ -34,7 +41,12 @@ public class Game {
     private final Scanner scan;
 
     /**
-     * Constructor de la classe Game.
+     * Constructor method of the Game class.
+     * @param nbPlayers Number of players.
+     * @param mode mode of the game
+     * @param scan Scanner.
+     * @param fileCardsPath path of file for cards
+     * @param fileTiles path of file for tiles.
      */
     public Game(int nbPlayers, String mode, Scanner scan, String fileCardsPath, String fileTilesPath) throws IOException{
         Objects.requireNonNull(scan);
@@ -60,12 +72,13 @@ public class Game {
         //import tuiles nobles aussi
         //System.out.println(this.cardStack);
         //System.out.println(this.cardStack);
-
+        
         initHashMapKeys(this.countersStack);
     }
-
     /**
-     * Methode qui initialise les tokens dans une hashmap
+     * Method to initialize the tokens in a HashMap 
+     * @param map Map of tokens 
+     * @return void
      */
     public void initHashMapKeys(HashMap<Token, Integer> map) {
         Objects.requireNonNull(map);
@@ -73,49 +86,59 @@ public class Game {
             map.put(Token.values()[i], 0);
         }
     }
-
     /**
-     * Methode pour trier une carte de développement en fonction de son niveau de jeu
+     * Method to sort a card according to its level
+     * @param level Level of the card to sort
+     * @param nbCardToExtract The maximum number of cards to be sorted 
+     * @return void
      */
     public void drawCardFromStack(int level, int nbCardToExtract) {
         var iter = this.cardStack.get(level).iterator();
         for (int i = 0; i < nbCardToExtract + 1; i++) {
             DevCard card = iter.next();
             var lst = this.discoveryCards.putIfAbsent(level, new ArrayList<DevCard>());
-            if (lst != null) {lst.add(card);}
+            if (lst != null) {lst.add(card);} 
             iter.remove();
         }
     }
 
     /**
-     * Methode pour ajouter un joueur dans la liste des joueurs du jeu
+     * Method to add a player in the list of all the players of the game.
+     * @param player Player to add in the list.
+     * @return void
      */
     public void addPlayerToGame(Player player) {
         Objects.requireNonNull(player);
         if (this.players.size() > this.nbPlayers) {
-            throw new IndexOutOfBoundsException("Number for Players is maximum");
+            throw new IndexOutOfBoundsException("Number for Players is maximum"); 
         }
         this.players.add(player);
     }
-
     /**
-     * Methode pour ajouter un nombre n de tokens dans la pile de jeu
+     * Method to add n tokens in the stack of tokens of the game.
+     * @param token Token(s) to add in the stack.
+     * @param n Number of tokens to add in the stack.
+     * @return void
      */
     public void addTokensToGame(Token counter, int n) {
         Objects.requireNonNull(counter);
         this.countersStack.put(counter, this.countersStack.get(counter) + n);
     }
-
     /**
-     * Methode pour retirer un nombre n de tokens de la pile de jeu
+     * Method to remove n tokens in the stack of tokens of the game. 
+     * @param token Token(s) to remove from the stack of tokens.
+     * @param n Number of tokens to remove from the stack.
+     * @return void
      */
     public void removeCountersFromGame(Token counter, int n) {
         Objects.requireNonNull(counter);
         this.countersStack.put(counter, this.countersStack.get(counter) - n);
     }
-
     /**
-     * Methode pour compter et comparer un nombre de tokens par rapport a un nombre donné maximum
+     * Method for counting and comparing a number of tokens according to a given maximum.
+     * @param player A player of the game.
+     * @param defaultValue Default number of tokens.
+     * @return True, if the player has more or same number of defaultValue of tokens, false otherwise.
      */
     private boolean verifyNumberCounters(Player player, int defaultValue){
         Objects.requireNonNull(player);
@@ -125,10 +148,10 @@ public class Game {
         }
         return sum < defaultValue;
     }
-
     /**
      * Method verify if the player can win according to his number of points.
-     * Methode permettant de verifier si un joueur peut gagner en fonction du nombre de point qu'il a
+     * @param player A player of the game.
+     * @return True, if the player can win, false otherwise.
      */
     public boolean victory(Player player) {
         Objects.requireNonNull(player);
@@ -137,14 +160,14 @@ public class Game {
         };
         return false;
     }
-
     /**
-     * Methode permettant de donner le vainqueur de la partie de jeu
+     * Method that gives the winner of the game.
+     * @return The winner.
      */
     public Player winner() {
         Player winnerOfGame = this.players.get(0);
         for (int i = 0; i < this.players.size(); i++){
-            var playerActual = this.players.get(i);
+            var playerActual = this.players.get(i); 
             if (victory(playerActual)) {
                 if (playerActual.getPlayerCards().size() < winnerOfGame.getPlayerCards().size()) {
                     winnerOfGame = playerActual;
@@ -155,18 +178,20 @@ public class Game {
     }
 
     /**
-     * Methode permettant de gerer l'action d'achat de deux tokens de meme couleur parmi les tokens présent dans la pile de jeu
+     * Method to manage the action of taking two same tokens from the deck of the game.
+     * @param player A player.
+     * @return True, if the player has took the tokens from the stack of the game, false otherwise.
      */
     public boolean actionSameCounters(Player player) {
         Objects.requireNonNull(player);
 
         if (!verifyNumberCounters(player, 10)) {
-            System.out.println("> Vous avez plus de 10 jetons deja !");
+            System.out.println("> Vous avez plus de 10 jetons deja !"); 
             return false;
         }
         var userChoice = Import.inputStr(this.scan, Shell.LISTCOLORSSTRING,"Vous avez choisi de prendre 2 jetons de la même couleur : Veuillez indiquer la couleur : ");
         if (this.countersStack.get(Token.values()[Shell.LISTCOLORSSTRING.indexOf(userChoice)]) < 3) {
-            System.out.println("Il n y a pas assez de jetons dans la pile");
+            System.out.println("Il n y a pas assez de jetons dans la pile"); 
             return false;
         }
         player.addToken(Token.values()[Shell.LISTCOLORSSTRING.indexOf(userChoice)], 2);
@@ -175,13 +200,15 @@ public class Game {
     }
 
     /**
-     * Methode permettant de gerer l'action d'achat de trois tokens de couleur différentes de la pile de jeu
+     * Method to manage the action of taking three different tokens from the deck of the game.
+     * @param player A player.
+     * @return True, if the player has took the tokens from the stack of the game, false otherwise.
      */
     public boolean actionDiffCounters(Player player, int nbCounters) {
         Objects.requireNonNull(player);
 
         if (!verifyNumberCounters(player, 10)) {
-            System.out.println("> Vous avez plus de 10 jetons deja !");
+            System.out.println("> Vous avez plus de 10 jetons deja !"); 
             return false;
         }
         boolean noProblem = true;
@@ -189,8 +216,8 @@ public class Game {
         for (int i = 0; i < nbCounters; i++) {
             var userChoice = Import.inputStr(this.scan, Shell.LISTCOLORSSTRING,"> Vous avez choisi de prendre " + (nbCounters - i) + " jetons différents : Veuillez indiquer la couleur: ");
             if (this.countersStack.get(Token.values()[Shell.LISTCOLORSSTRING.indexOf(userChoice)]) < 1 || temp.contains(Token.values()[Shell.LISTCOLORSSTRING.indexOf(userChoice)])) {
-                System.out.println("> Il n y a pas assez de jetons dans la pile\n> Veuillez mettre 3 jetons différents");
-                noProblem = false;
+                System.out.println("> Il n y a pas assez de jetons dans la pile\n> Veuillez mettre 3 jetons différents"); 
+                noProblem = false; 
                 break;
             }
             temp.add(Token.values()[Shell.LISTCOLORSSTRING.indexOf(userChoice)]);
@@ -201,10 +228,10 @@ public class Game {
         }
         return noProblem;
     }
-
     /**
-     * Methode permettant de gerer l'action d'achat d'une carte de développement, de la pile de jeu (parmi les cartes découvertes)
-     *  ou dans les cartes reservées du joueur (s'il en a)
+     * Method to manage the action of buying a development card from the discovery cards of the game or from the list of reserved cards of the player.
+     * @param player A player.
+     * @return True, if the player has buy the card, false otherwise.
      */
     public boolean actionBuyCard(Player player) {
         Objects.requireNonNull(player);
@@ -219,7 +246,7 @@ public class Game {
         if (payCard == null || !payCard.equals(Map.of(Token.values()[0], -1))) {
             // la carte peut deja etre payé avec les bonus ou jetons
             player.buyCard(cardUser);
-            this.discoveryCards.get(cardUser.getLevel()).remove(cardUser);
+            this.discoveryCards.get(cardUser.getLevel()).remove(cardUser);            
             this.drawCardFromStack(cardUser.getLevel(), 1);
             if (payCard != null){
                 //la carte doit etre payé avec un reste de jetons
@@ -236,8 +263,9 @@ public class Game {
         }
     }
     /**
-     * Methode permettant de gerer l'action d'achat d'une carte de developpement parmi les cartes reservées du joueur
-     *  (ajouter la carte a la liste des cartes du joueur)
+     * Method to manage the action of buying a reserved card from the list of the player.
+     * @param player A player.
+     * @return True, if the card has been buy by the player, false otherwise.
      */
     public boolean actionBuyReserveCard(Player player) {
         Objects.requireNonNull(player);
@@ -249,7 +277,7 @@ public class Game {
         }
         int numberCard = Import.inputInt(this.scan, "Choisissez quel carte réservé vous voulez acheter [1-" + player.getReservedCards().size() +"]", player.getReservedCards().size());
         var card = player.getReservedCards().get(numberCard - 1);
-        var payCard = player.leftToPay(card);
+        var payCard = player.leftToPay(card); 
         if (payCard == null || !payCard.equals(Map.of(Token.values()[0], -1))) {
             player.buyCard(card);
             player.removeReservedCard(card);
@@ -266,8 +294,9 @@ public class Game {
         }
     }
     /**
-     * Methode permettant de gerer l'action de reservation d'une carte de développement parmi celles a découvert sur le plateau
-     * de jeu ou dans la pioche (à l'aveugle)
+     * Method to manage the action of reserved a card from the discovery cards of the game or from the stack of cards.
+     * @param player A player.
+     * @return True, if the player has reserved the card, false otherwise.
      */
     public boolean actionReserveCard(Player player) {
         Objects.requireNonNull(player);
@@ -295,8 +324,12 @@ public class Game {
         return true;
     }
 
+
     /**
-     * Methode permettant de gerer un tour de jeu
+     * Method to manage a game turn 
+     * @param player A player.
+     * @param allActionsAvailables List of all the actions that the player can choose.
+     * @return void
      */
     public void playRound(Player player) {
         Objects.requireNonNull(player);
@@ -308,38 +341,42 @@ public class Game {
             //var userChoice = Import.inputStr(this.scan, new String());
             var userChoice = Import.inputStr(this.scan, this.rules.getListActions(), "> Choisissez : ");
             switch (userChoice) {
-                case ACTIONSAMECOUNTER:
+                case ACTIONSAMECOUNTER: 
                     noPlay = this.actionSameCounters(player); break;
-                case ACTIONDIFFCOUNTER:
+                case ACTIONDIFFCOUNTER: 
                     noPlay = this.actionDiffCounters(player, 3); break;
-                case ACTIONBUYCARD:
+                case ACTIONBUYCARD:   
                     noPlay = this.actionBuyCard(player); break;
-                case ACTIONRESERVECARD:
+                case ACTIONRESERVECARD:   
                     noPlay = this.actionReserveCard(player);break;
-                case PRINTMYCARDS:
+                case PRINTMYCARDS:      
                     System.out.println(player.getPlayerCards()); break;
-                case PRINTMYTOKENS:
+                case PRINTMYTOKENS:   
                     System.out.println(Shell.toStringTokenLongFormat(player.getTokensAvailables(), "\n")); break;
                 default:
                     break;
             }
         }
     }
-
     /**
-     * Methode permettant d'initialiser un plateau de jeu
+     * Method to initialize the game board
+     * @param nbLevel A level.
+     * @param nbCardByLevel The number of card for each level.
+     * @param nbTokens The number of token.
+     * @return void
      */
     public void initGame(int nbLevel, int nbCardByLevel, int nbTokens) {
         int i;
-        for (i = 0; i < nbTokens; i++)
-        {this.addTokensToGame(Token.values()[i], 2 + this.nbPlayers);}
-        for (i = 0; i < nbLevel; i++)
-        {this.drawCardFromStack(i + 1, nbCardByLevel);
+        for (i = 0; i < nbTokens; i++) 
+            {this.addTokensToGame(Token.values()[i], 2 + this.nbPlayers);}
+        for (i = 0; i < nbLevel; i++) 
+            {this.drawCardFromStack(i + 1, nbCardByLevel);
         }
     }
-
     /**
-     * Methode permettant de gerer le jeu (le gagnant, les tours ... )
+     * Method to manage a game.
+     * @param phaseChoice The choice of the game phase (phase 1 will be played with 2 players)
+     * @return void
      */
     public void playGame(String phaseChoice) {
 
@@ -347,11 +384,11 @@ public class Game {
             throw new IllegalAccessError("Missing Players to Play a Game");
         }
         initGame(this.rules.getNbLevel(), this.rules.getNbCardsByLevel(), this.rules.getNbTokens());
-        boolean playing = true;
+        boolean playing = true;        
         while (playing) {
             for (int i = 0; i < this.players.size(); i++) {
                 var playerActual = this.players.get(i);
-                if (this.victory(playerActual)) {playing = false;}
+                if (this.victory(playerActual)) {playing = false;} 
                 System.out.println(this);
                 System.out.println(playerActual);
                 this.playRound(playerActual);
@@ -360,41 +397,55 @@ public class Game {
         winner();
         System.out.println(this.classement(this.players));
     }
-
     /**
-     * Methode permettant de classer les joueurs du jeu (en fonction des points acquis)
+     * Method for ranking players at the end of the game.
+     * @param players The list of the players of the game.
+     * @return The classement of the game.
      */
     public String classement(ArrayList<Player> players) {
         Stream <String> streamClassement = players.stream()
-                .sorted(Comparator.comparingInt(Player::getAllPoints)
-                        .reversed()).map(player -> "> Joueur : " + player.getNamePlayer() + " : " + player.getAllPoints());
-        var result = streamClassement.collect(Collectors.joining("\n"));
+            .sorted(Comparator.comparingInt(Player::getAllPoints)
+            .reversed()).map(player -> "> Joueur : " + player.getNamePlayer() + " : " + player.getAllPoints());
+        var result = streamClassement.collect(Collectors.joining("\n"));  
         return "==============\n> Classement\n==============\n" + result;
     }
-
-    @Override
+    /**
+     * Method rewriting equals for an Object of the class Game
+     * @param other object quelconcque.
+     * @return vrai, si les champs sont les mêmes, sinon faux 
+     */
+    @Override 
     public boolean equals(Object o){
         Objects.requireNonNull(o);
 
         return o instanceof Game g
-                && this.countersStack.equals(g.countersStack)
-                && this.discoveryCards.equals(g.discoveryCards);
+        && this.countersStack.equals(g.countersStack)
+        && this.discoveryCards.equals(g.discoveryCards);
     }
 
-
-    @Override
+    /**
+     * Method rewriting hashCode for an Object of the class Game
+     * @return entier correspondant à l'objet.
+     */
+    @Override 
     public int hashCode(){
         return Objects.hash(this.countersStack, this.discoveryCards);
     }
-
-    @Override
+    /**
+     * Method rewriting toString for an Object of the class Game
+     * @return chaine de caractère associés à l'affichage
+     */
+    @Override 
     public String toString(){
         return "----------- On the board of the game -----------\n"
-                + "Tiles from the deck :\n" + (this.discoveryTiles == null ? "" : this.discoveryTiles) + "\n"
-                + "Cards from the deck : " + this.discoveryCards + "\n"
-                + "Counters from the deck :\n" + Shell.toStringTokenLongFormat(this.countersStack, "\n");
+        + "Tiles from the deck :\n" + (this.discoveryTiles == null ? "" : this.discoveryTiles) + "\n"
+        + "Cards from the deck : " + this.discoveryCards + "\n"
+        + "Counters from the deck :\n" + Shell.toStringTokenLongFormat(this.countersStack, "\n");
     }
-
+    /**
+     * Getter method for an Object of type Game.
+     * @return Number of players.
+     */
     public int getNbPlayers() {
         return this.players.size();
     }
