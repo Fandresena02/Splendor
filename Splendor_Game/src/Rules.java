9848123003d296d725e7d22553408e279384c86f
christine-jl <76869239+christine-jl@users.noreply.github.com>
2022-12-17 15:15:15 +0100

    Update Rules.java
    modification class rules
    Add some more methods

--- Splendor_Game/src/Rules.java
+++ Splendor_Game/src/Rules.java
@@ -1,8 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-
+/**
+ * Class Rules, manage rules for Game.
+ *
+ * @author Christine LI - Clement LUC - Fandresena RAKOTOMAHEFA
+ * @date 18/12/2022
+ * @file Rules.java
+ */
 public class Rules {
+
     private final int pointsForVictory = 15;
     private final int maxPlayers = 3;
 
@@ -22,7 +29,13 @@ public class Rules {
         List.of("Ajout de 2 Mêmes jetons", "Ajout de 3 jetons Différents", "Achat d'une Carte", 
         "Affichage de mes cartes", "Affichage de mes Jetons", "Reserve d'une Carte")); 
 
-
+    /**
+     * Constructor method of the Rules class.
+     * @param nbLevel number of levels in the game
+     * @param nbCardByLevel number of cards for each level
+     * @param nbTokens number of tokens from each color
+     * @param mode mode of the Game
+     */
     public Rules(int nbLevel, int nbCardByLevel, int nbTokens, String mode) {
         Objects.requireNonNull(mode);
 
@@ -32,8 +45,8 @@ public class Rules {
         this.mode = mode;
     }
     /**
-     * Method to guide the player in his choice of action during the game
-     * Methode permettant de guider le joueur dans son choix d'action au cours du jeu
+     * Method to guide the player in his choice of action during the game.
+     * @return String of the action choosen
      */
     public String helpUser() {
         StringBuilder strb = new StringBuilder();
@@ -43,9 +56,9 @@ public class Rules {
         }
         return strb.toString();
     }
-
     /**
-     * Methode permettant de retirer une action du jeu
+     * Méthod for removing an action from the game
+     * @param actionToRemove action to remove
      */
     public void removeActions(String actionToRemove) {
         var index = this.listActions.indexOf(actionToRemove);
@@ -55,9 +68,10 @@ public class Rules {
         this.listActions.remove(actionToRemove);
         this.listDescriptionActions.remove(index);
     }
-
     /**
-     * Methode qui initialise les regles du jeu
+     * Method initializing rules of the game 
+     * @param mode the choosen mode
+     * @return new Rules for the Game
      */
     public static Rules initRulesFromString(String mode) {
         if (mode.equals(phase1)) {
@@ -70,7 +84,11 @@ public class Rules {
         }
         throw new IllegalArgumentException("mode not accepted");
     }
-
+    /**
+     * Method rewriting equals for an Object of class Rules.
+     * @param other An object 
+     * @return True, if the fields are the same, false otherwise. 
+     */
     @Override
     public boolean equals(Object other) {
         Objects.requireNonNull(other);
@@ -82,31 +100,53 @@ public class Rules {
         && this.maxPlayers == r.maxPlayers
         && this.mode.equals(r.mode);
     }
-
+    /**
+     * Method rewriting hashCode for an Object of the class Rules.
+     * @return Integer corresponding to the object.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(this.nbLevel, this.nbTokens, this.pointsForVictory, this.nbCardByLevel, this.mode, this.maxPlayers);
     }
-
+    /**
+     * Getter method for an Object of type Rules.
+     * @return Number of Levels.
+     */
     public int getNbLevel() {
         return this.nbLevel;
     }
-
+    /**
+     * Getter method for an Object of type Rules.
+     * @return Number of Levels.
+     */
     public int getNbCardsByLevel() {
         return this.nbCardByLevel;
     }
-
+    /**
+     * Getter method for an Object of type Rules.
+     * @return Number of tokens for each color.
+     */
     public int getNbTokens() {
         return this.nbTokens;
     }
-
+    /**
+     * Getter method for an Object of type Rules.
+     * @return mode of the game.
+     */
     public String getMode() {
         return this.mode;
     }
-
+    /**
+     * Getter method for an Object of type Rules.
+     * @return Maximum player for the Game.
+     */
     public int getMaxPlayers() {
         return this.maxPlayers;
     }
+    /**
+     * Getter method for an Object of type Rules.
+     * @return all Actions availables.
+     */
     public List<String> getListActions() {
         return this.listActions;
     }
