b8d8b2a1c95900ed1d0d58f03ec39465ca261b96
christine-jl <76869239+christine-jl@users.noreply.github.com>
2022-12-17 15:16:37 +0100

    Update Token.java
    modification of the class token
    modify some comment

--- Splendor_Game/src/Token.java
+++ Splendor_Game/src/Token.java
@@ -1,11 +1,19 @@
 import java.awt.Color;
-import java.awt.geom.Ellipse2D;
 
+/**
+ * Class defining a Token in the Splendor game
+ * An object of type Token is symbolized by its color : int.
+ * @author Christine LI - Clement LUC - Fandresena RAKOTOMAHEFA
+ * @date 18/12/2022
+ * @file Token.java
+ */
 public enum Token {
+
     EMERAUDE,
     DIAMANT,
     SAPHIR,
     ONYX,
     RUBIS,
     OR;
+
 }
